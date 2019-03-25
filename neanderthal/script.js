e5e7d7cb6ad3fdad0b1c2c292c39a38ea841240a
Chris Robinson <chrobins@ucsd.edu>
2019-03-24 22:43:07 -0700

    First functional website commit

---
+++ neanderthal/script.js
@@ -0,0 +1,31 @@
+function hoverSoundCloud(element) {
+  element.setAttribute('src', 'img/soundcloudhover.png');
+}
+
+function unhoverSoundCloud(element) {
+  element.setAttribute('src', 'img/soundcloud.png');
+}
+
+function hoverSpotify(element) {
+  element.setAttribute('src', 'img/Spotifyhover.jpg');
+}
+
+function unhoverSpotify(element) {
+  element.setAttribute('src', 'img/Spotify.jpg');
+}
+
+function hoverTwitter(element) {
+  element.setAttribute('src', 'img/twitterhover.png');
+}
+
+function unhoverTwitter(element) {
+  element.setAttribute('src', 'img/twitter.png');
+}
+
+function hoverInsta(element) {
+  element.setAttribute('src', 'img/instahover.jpg');
+}
+
+function unhoverInsta(element) {
+  element.setAttribute('src', 'img/insta.jpeg');
+}
