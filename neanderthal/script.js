7e00a7f23da53ea565a4823b344320d0bb008cca
Chris Robinson <chrobins@ucsd.edu>
2019-07-17 17:52:49 -0700

    0x0 bug fix attempt8

--- neanderthal/script.js
+++ neanderthal/script.js
@@ -1,51 +1,38 @@
 //script for dynamically resizing background image
-window.onload = function() {
-
-    var imageSrc = document
-                    .body
-                     .style
-                      .backgroundImage
-                       .replace(/url\((['"])?(.*?)\1\)/gi, '$2')
-                        .split(',')[0];
-
-    var image = new Image();
-    image.src = imageSrc;
-    var width = image.width,
-        height = image.height;
-    alert('width =' + width + ', height = ' + height)    
-
-}
-
-var ratio = height / width;
+var size, img, width, height, ratio;
+img = new Image();
+img.src = document.body.style.backgroundImage.replace(/url\((['"])?(.*?)\1\)/gi, '$2').split(',')[0];
+console.log(img.src);
+width = img.width;
+height = img.height;
+ratio = height / width;
+console.log(document.body.style.backgroundSize);
 document.body.style.backgroundRepeat = 'no-repeat';
 document.body.style.backgroundAttachment = 'fixed';
-var size = window.innerWidth + "px";
+size = window.innerWidth + "px";
 document.body.style.backgroundSize = size;
+console.log(ratio + " " + width + " " + height );
 
 resizeBG();
 
 function resizeBG(){
+console.log(ratio + " " + width + " " + height );
   if(width == 0 || height == 0){
-    console.log("are u even going here? " + img.width + " x " + height);
-    var width = img.width;
-    var height = img.height;
-    var ratio = height / width;
+    width = img.width;
+    height = img.height;
+    ratio = height / width;
   }
 
   var currentRatio = window.innerHeight/window.innerWidth;
  // document.body.style.backgroundPosition = 'center';
 
   if(currentRatio < ratio){     //if height is less than width
-    console.log("height < width");
-    var size = window.innerWidth + "px";
+    size = window.innerWidth + "px";
     document.body.style.backgroundSize = size;
   }
 
   else{                         //if width is less than height
-    console.log("height > width");
-    console.log(window.innerHeight + " x " +  window.innerWidth);
-    console.log("og: " + height + " x " +  width);
-    var size = "auto " + window.innerHeight + "px";
+    size = "auto " + window.innerHeight + "px";
     document.body.style.backgroundSize = size;
   }
 }
--- script.js
+++ script.js
@@ -1,35 +1,38 @@
 //script for dynamically resizing background image
-
-var img = new Image();
-img.src = document.body.style.backgroundImage.replace(/url\(['"]?(.*)['"]?\)/gi, '$1');
-var width = img.width;
-var height = img.height;
-var ratio = height / width;
+var size, img, width, height, ratio;
+img = new Image();
+img.src = document.body.style.backgroundImage.replace(/url\((['"])?(.*?)\1\)/gi, '$2').split(',')[0];
+console.log(img.src);
+width = img.width;
+height = img.height;
+ratio = height / width;
 console.log(document.body.style.backgroundSize);
 document.body.style.backgroundRepeat = 'no-repeat';
 document.body.style.backgroundAttachment = 'fixed';
-var size = window.innerWidth + "px";
+size = window.innerWidth + "px";
 document.body.style.backgroundSize = size;
+console.log(ratio + " " + width + " " + height );
 
 resizeBG();
 
 function resizeBG(){
+console.log(ratio + " " + width + " " + height );
   if(width == 0 || height == 0){
-    var width = img.width;
-    var height = img.height;
-    var ratio = height / width;
+    width = img.width;
+    height = img.height;
+    ratio = height / width;
   }
 
   var currentRatio = window.innerHeight/window.innerWidth;
  // document.body.style.backgroundPosition = 'center';
 
   if(currentRatio < ratio){     //if height is less than width
-    var size = window.innerWidth + "px";
+    size = window.innerWidth + "px";
     document.body.style.backgroundSize = size;
   }
 
   else{                         //if width is less than height
-    var size = "auto " + window.innerHeight + "px";
+    size = "auto " + window.innerHeight + "px";
     document.body.style.backgroundSize = size;
   }
 }
